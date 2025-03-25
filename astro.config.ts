6206bf511c0aed2e7e2c3ef2b2bb64bc5ffbdeca
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2025-03-25 00:39:23 +0000

    Yay

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import icon from "astro-icon";
 
 export default defineConfig({
     integrations: [
         UnoCSS({
             injectReset: true,
         }),
+        icon(),
     ],
 });
---
+++ src/lib/rinth.ts
@@ -0,0 +1,5 @@
+import { Modrinth } from "typerinth";
+
+export const modrinth = new Modrinth({
+    userAgent: "TheBrokenSite/main",
+});
