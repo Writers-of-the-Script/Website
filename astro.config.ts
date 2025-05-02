1ec220b3c6d219d3adbe92d2d88f7644cf7f7a91
Jacob Sapoznikow <jacob1coder@gmail.com>
2025-05-02 19:44:39 +0000

    More docs!

--- astro.config.ts
+++ astro.config.ts
@@ -33,147 +33,143 @@ export default defineConfig({
             : {},
     },
 
-    integrations: [
-        UnoCSS({
-            injectReset: true,
-        }),
-        starlight({
-            title: "The Broken Script",
-            favicon: "/logo.png",
-            description: "Here I am.",
-            tagline: "The World They Built. The World They Hate.",
-            // routeMiddleware: "./src/routeData.ts",
+    integrations: [UnoCSS({
+        injectReset: true,
+    }), starlight({
+        title: "The Broken Script",
+        favicon: "/logo.png",
+        description: "Here I am.",
+        tagline: "The World They Built. The World They Hate.",
+        // routeMiddleware: "./src/routeData.ts",
 
-            logo: {
-                src: "./src/assets/logo.png",
+        logo: {
+            src: "./src/assets/logo.png",
+        },
+
+        customCss: ["./src/styles/docs.css"],
+
+        sidebar: [
+            {
+                label: "Wiki",
+                items: [
+                    { label: "[tabler:home-filled] Home", link: "/wiki/" },
+                    { label: "[tabler:search] ARG", link: "/wiki/arg" },
+                    {
+                        label: "[tabler:terminal] The Broken Script",
+                        link: "/wiki/tbs",
+                    },
+                    {
+                        label: "[tabler:alert-hexagon-filled] Spectrum 11",
+                        link: "/wiki/spectrum-11",
+                    },
+                    {
+                        label: "[tabler:sitemap-filled] Items",
+                        link: "/wiki/items",
+                    },
+                ],
             },
 
-            customCss: ["./src/styles/docs.css"],
-
-            sidebar: [
-                {
-                    label: "Wiki",
-                    items: [
-                        { label: "[tabler:home-filled] Home", link: "/wiki/" },
-                        { label: "[tabler:search] ARG", link: "/wiki/arg" },
-                        {
-                            label: "[tabler:terminal] The Broken Script",
-                            link: "/wiki/tbs",
-                        },
-                        {
-                            label: "[tabler:alert-hexagon-filled] Spectrum 11",
-                            link: "/wiki/spectrum-11",
-                        },
-                        {
-                            label: "[tabler:sitemap-filled] Items",
-                            link: "/wiki/items",
-                        },
-                    ],
-                },
-
-                {
-                    label: "[tabler:info-circle-filled] Information",
-                    autogenerate: { directory: "wiki/info" },
-                },
-
-                {
-                    label: "[tabler:book-filled] Lore",
-                    autogenerate: { directory: "wiki/lore" },
-                },
-
-                {
-                    label: "[tabler:settings-filled] Mechanics",
-                    autogenerate: { directory: "wiki/mechanics" },
-                },
-
-                {
-                    label: "[tabler:stack-3-filled] Dimensions",
-                    autogenerate: { directory: "wiki/dimensions" },
-                },
-
-                {
-                    label: "[tabler:buildings] Structures",
-                    autogenerate: { directory: "wiki/structures" },
-                },
-
-                {
-                    label: "[tabler:ghost-3-filled] Entities",
-                    autogenerate: { directory: "wiki/entities" },
-                },
-
-                {
-                    label: "[tabler:code] Documentation",
-                    autogenerate: { directory: "docs", collapsed: true },
-                },
-            ],
-
-            social: {
-                github: "https://github.com/RedstoneWizard08/TheBrokenSite",
-                discord: "https://discord.gg/nullnullnullnull",
+            {
+                label: "[tabler:info-circle-filled] Information",
+                autogenerate: { directory: "wiki/info" },
             },
 
-            editLink: {
-                baseUrl:
-                    "https://github.com/RedstoneWizard08/TheBrokenSite/edit/main/",
+            {
+                label: "[tabler:book-filled] Lore",
+                autogenerate: { directory: "wiki/lore" },
             },
 
-            tableOfContents: {
-                maxHeadingLevel: 4,
+            {
+                label: "[tabler:settings-filled] Mechanics",
+                autogenerate: { directory: "wiki/mechanics" },
             },
 
-            plugins: [
-                imageZoom(),
-                kbd({
-                    types: [{ id: "shortcut", label: "Key", default: true }],
-                }),
-                mdBlocks({
-                    blocks: {
-                        // Config
-                    },
-                }),
-                ion({
-                    icons: {
-                        include: {
-                            tabler: ["*"],
-                        },
-                    },
+            {
+                label: "[tabler:stack-3-filled] Dimensions",
+                autogenerate: { directory: "wiki/dimensions" },
+            },
 
-                    overrides: {
-                        Head: false,
-                    },
-                }),
-                {
-                    name: "ion-overrides-fixer",
-                    hooks: {
-                        setup: ({ config, updateConfig }) => {
-                            updateConfig({
-                                components: {
-                                    ...config.components,
-                                    Head: "./src/components/WikiHead.astro",
-                                    Sidebar:
-                                        "./src/components/WikiSidebar.astro",
-                                    SiteTitle:
-                                        "./src/components/WikiSiteTitle.astro",
-                                    EditLink:
-                                        "./src/components/WikiEditButton.astro",
-                                    Pagination:
-                                        "./src/components/WikiPagination.astro",
-                                    Header: "./src/components/WikiHeader.astro",
-                                    MobileMenuFooter:
-                                        "./src/components/WikiMobileMenuFooter.astro",
-                                },
-                            });
-                        },
+            {
+                label: "[tabler:buildings] Structures",
+                autogenerate: { directory: "wiki/structures" },
+            },
+
+            {
+                label: "[tabler:ghost-3-filled] Entities",
+                autogenerate: { directory: "wiki/entities" },
+            },
+
+            {
+                label: "[tabler:code] Documentation",
+                autogenerate: { directory: "docs" },
+            },
+        ],
+
+        social: {
+            github: "https://github.com/RedstoneWizard08/TheBrokenSite",
+            discord: "https://discord.gg/nullnullnullnull",
+        },
+
+        editLink: {
+            baseUrl:
+                "https://github.com/RedstoneWizard08/TheBrokenSite/edit/main/",
+        },
+
+        tableOfContents: {
+            maxHeadingLevel: 4,
+        },
+
+        plugins: [
+            imageZoom(),
+            kbd({
+                types: [{ id: "shortcut", label: "Key", default: true }],
+            }),
+            mdBlocks({
+                blocks: {
+                    // Config
+                },
+            }),
+            ion({
+                icons: {
+                    include: {
+                        tabler: ["*"],
                     },
                 },
-                validator({
-                    exclude: ["/downloads", "/javadoc"],
-                }),
-            ],
-        }),
-        (await import("@playform/compress")).default({
-            Image: false,
-            Exclude: ["/javadoc"],
-        }),
-    ],
+
+                overrides: {
+                    Head: false,
+                },
+            }),
+            {
+                name: "ion-overrides-fixer",
+                hooks: {
+                    setup: ({ config, updateConfig }) => {
+                        updateConfig({
+                            components: {
+                                ...config.components,
+                                Head: "./src/components/WikiHead.astro",
+                                Sidebar:
+                                    "./src/components/WikiSidebar.astro",
+                                SiteTitle:
+                                    "./src/components/WikiSiteTitle.astro",
+                                EditLink:
+                                    "./src/components/WikiEditButton.astro",
+                                Pagination:
+                                    "./src/components/WikiPagination.astro",
+                                Header: "./src/components/WikiHeader.astro",
+                                MobileMenuFooter:
+                                    "./src/components/WikiMobileMenuFooter.astro",
+                            },
+                        });
+                    },
+                },
+            },
+            validator({
+                exclude: ["/downloads", "/javadoc"],
+            }),
+        ],
+    }), (await import("@playform/compress")).default({
+        Image: false,
+        Exclude: ["/javadoc"],
+    })],
 });
